111c94c58b9d1206a464abd63aedf4ba6e1675a5
Tomás Martins <tomas.marti@me.com>
2023-04-27 17:50:52 -0300

    Limit platforms

--- Package.swift
+++ Package.swift
@@ -5,22 +5,21 @@ import PackageDescription
 
 let package = Package(
     name: "Giffy",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Giffy",
             targets: ["Giffy"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/Flipboard/FLAnimatedImage.git", .upToNextMajor(from: "1.0.17"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Giffy",
-            dependencies: []),
+            dependencies: ["FLAnimatedImage"]),
         .testTarget(
             name: "GiffyTests",
             dependencies: ["Giffy"]),
--- Sources/Giffy/Giffy.swift
+++ Sources/Giffy/Giffy.swift
@@ -1,6 +1,16 @@
-public struct Giffy {
-    public private(set) var text = "Hello, World!"
+//
+//  Giffy.swift
+//  GuruDesignSystem
+//
+//  Created by Tomas Martins on 27/04/23.
+//  Copyright © 2023 Tomas Martins. All rights reserved.
+//
 
-    public init() {
-    }
+import UIKit
+import SwiftUI
+import FLAnimatedImage
+
+@available(iOS 13.0, *)
+public struct Giffy: UIViewRepresentable {
+
 }
